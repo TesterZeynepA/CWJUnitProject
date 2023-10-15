68ca46eb861c471b36cb6005fbaeef02ac15e25c
ZeynepA <zeyneppaytop@gmail.com>
2023-10-15 23:22:17 +0300

    CWJUnit Project 15.10.2023 commit

---
+++ src/test/java/Homework/Task05.java
@@ -0,0 +1,64 @@
+package Homework;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task05 {
+   /** Navigate to website https://testpages.herokuapp.com/styled/index.html
+    Under the ORIGINAL CONTENTS
+    Click on Alerts
+    Print the URL
+    Navigate back
+    Print the URL
+    Click on Basic Ajax
+    Print the URL
+    Enter value → 20 and Enter
+    And then verify Submitted Values is displayed open page
+    Close driver. */
+
+   static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws InterruptedException {
+
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://automationpractice.com/index.php");
+
+        Thread.sleep(3000);
+
+    }
+
+    @Test
+    public void xPathAbsolute() throws InterruptedException {
+
+        WebElement userNameBoxForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+
+        userNameBoxForAbsolute.click();
+
+        Thread.sleep(3000);
+
+
+        WebElement passwordBoxForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+
+        passwordBoxForAbsolute.click();
+
+        Thread.sleep(3000);
+
+        WebElement loginButtonForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+
+        loginButtonForAbsolute.click();
+
+        Thread.sleep(3000);
+
+
+    }
+
+
+
+
+}

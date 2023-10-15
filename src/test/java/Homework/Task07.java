421ac799632ec5f493a925ddc633102a4dd18254
ZeynepA <zeyneppaytop@gmail.com>
2023-10-15 23:25:47 +0300

    CWJUnit Project 15.10.2023 commit

--- src/test/java/Homework/Task07.java
+++ src/test/java/Homework/Task07.java
@@ -1,5 +1,12 @@
 package Homework;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task07 {
 
   /**  Go to http://www.bestbuy.com
@@ -7,6 +14,43 @@ public class Task07 {
             LogoTest → Verify if the BestBuy logo is displayed.
             CanadaLinkTest → Verify if the link is displayed. */
 
+  static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws InterruptedException {
+
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://automationpractice.com/index.php");
+
+        Thread.sleep(3000);
+
+    }
+
+    @Test
+    public void xPathAbsolute() throws InterruptedException {
+
+        WebElement userNameBoxForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+
+        userNameBoxForAbsolute.click();
+
+        Thread.sleep(3000);
+
+
+        WebElement passwordBoxForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+
+        passwordBoxForAbsolute.click();
+
+        Thread.sleep(3000);
+
+        WebElement loginButtonForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+
+        loginButtonForAbsolute.click();
+
+        Thread.sleep(3000);
+
+
+    }
 
 
 
--- src/test/java/Homework/Task08.java
+++ src/test/java/Homework/Task08.java
@@ -1,5 +1,12 @@
 package Homework;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task08 {
 
 
@@ -13,6 +20,43 @@ public class Task08 {
  Alışverişi tamamlayınız.
  Son olarak "Teknosa’ya Hoş Geldiniz" WebElement’inin text’ini yazdırınız.
  Driver’ı kapatınız. */
+static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws InterruptedException {
+
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://automationpractice.com/index.php");
+
+        Thread.sleep(3000);
+
+    }
+
+    @Test
+    public void xPathAbsolute() throws InterruptedException {
+
+        WebElement userNameBoxForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+
+        userNameBoxForAbsolute.click();
+
+        Thread.sleep(3000);
+
+
+        WebElement passwordBoxForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+
+        passwordBoxForAbsolute.click();
+
+        Thread.sleep(3000);
+
+        WebElement loginButtonForAbsolute = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+
+        loginButtonForAbsolute.click();
+
+        Thread.sleep(3000);
+
+
+    }
 
 
 

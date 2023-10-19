964639408cb2ede1bab9a827e96e027ed39632a1
ZeynepA <zeyneppaytop@gmail.com>
2023-10-19 17:41:09 +0300

    CWJUnit Project 19.10.2023

--- src/test/java/Homework/Task11.java
+++ src/test/java/Homework/Task11.java
@@ -1,5 +1,18 @@
 package Homework;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+import java.util.List;
+
 public class Task11 {
     /*
    Go to URL: https://the-internet.herokuapp.com/dropdown
@@ -10,7 +23,146 @@ public class Task11 {
    Verify the dropdown has Option 2 text.
    Create method printFirstSelectedOptionTest Print first selected option.
    Find the size of the dropdown, Print "Expected Is Not Equal Actual" if there are not 3 elements in the dropdown.
+
     */
 
+    static WebDriver driver;
 
+    @BeforeClass
+    public static void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
+    @Test
+    public void test() throws InterruptedException {
+
+        selectByIndexTest();
+
+        Thread.sleep(2000);
+
+        selectByValueTest();
+
+        Thread.sleep(2000);
+
+        selectByVisibleTextTest();
+
+        Thread.sleep(2000);
+
+        printAllTest();
+
+        Thread.sleep(2000);
+
+        hasOption2();
+
+        Thread.sleep(2000);
+
+        printFirstSelectedOptionTest();
+
+        Thread.sleep(2000);
+
+        FindTheSizeOfTheDropdown();
+    }
+
+    private void FindTheSizeOfTheDropdown() {
+        System.out.println("Method 7 Çalıştı");
+
+        WebElement dropDown = driver.findElement(By.id("dropdown"));
+
+        Select select = new Select(dropDown);
+
+        List<WebElement> selectList = select.getOptions();
+
+        int actualSelectSize = selectList.size();
+
+        int expectedSelectSize = 3;
+
+        System.out.println(actualSelectSize == expectedSelectSize ? "Expected is equal actual " : "Expected is not equal actual");
+    }
+
+    private void printFirstSelectedOptionTest() {
+        System.out.println("Method 6 Çalıştı");
+
+        WebElement dropDown = driver.findElement(By.id("dropdown"));
+
+        Select select = new Select(dropDown);
+
+        System.out.println("select.getFirstSelectedOption().getText() = " + select.getFirstSelectedOption().getText());
+
+    }
+
+    private void hasOption2() {
+        System.out.println("Method 5 Çalıştı");
+
+        WebElement dropDown = driver.findElement(By.id("dropdown"));
+
+        Select select = new Select(dropDown);
+
+        List <WebElement> selectAllList = select.getOptions();
+
+        for (WebElement element : selectAllList) {
+          if(element.getText().contains("Option 2")){
+              System.out.println("SelectAllList has Option 2");
+          }
+        }
+
+    }
+
+
+    private void printAllTest() {
+        System.out.println("Method 4 Çalıştı");
+
+        WebElement dropDown = driver.findElement(By.id("oldSelectMenu"));
+
+        Select select = new Select(dropDown);
+
+        List<WebElement> selectAllList = select.getOptions();
+
+        for (WebElement element : selectAllList){
+
+            System.out.println("element.getText() = " + element.getText());
+
+        }
+
+    }
+
+    private void selectByVisibleTextTest() {
+        System.out.println("Method 3 Çalıştı");
+
+        WebElement dropDown = driver.findElement(By.id("dropdown"));
+
+        Select select = new Select(dropDown);
+
+        select.selectByVisibleText("Option 1");
+
+    }
+
+    private void selectByValueTest() {
+        System.out.println("Method 2 Çalıştı");
+
+        WebElement dropDown = driver.findElement(By.id("dropdown"));
+
+        Select select = new Select(dropDown);
+
+        select.selectByValue("2");
+
+    }
+
+    private void selectByIndexTest() {
+
+        System.out.println("Method 1 Çalıştı");
+
+        WebElement dropDown = driver.findElement(By.id("dropdown"));
+
+        Select select = new Select(dropDown);
+
+        select.selectByIndex(1);
+
+    }
 }
--- src/test/java/Homework/Task12.java
+++ src/test/java/Homework/Task12.java
@@ -1,5 +1,17 @@
 package Homework;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+import java.util.List;
+
 public class Task12 {
     //https://demoqa.com/select-menu
     //Get all the options of the dropdown
@@ -9,5 +21,75 @@ public class Task12 {
     //Print FirstSelectedOptionTest
     //Select option "Yellow"
 
+   // Açılır menüdeki tüm seçenekleri alın
+    // Seçenek boyutu
+    // Tüm testi yazdır
+    // Açılır menüde "Siyah" seçeneğinin bulunduğunu doğrulayın
+    // FirstSelectedOptionTest Yazdır "Sarı" seçeneğini seçin
+
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://demoqa.com/select-menu");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+
+        WebElement dropDown = driver.findElement(By.id("oldSelectMenu"));
+
+        Select select = new Select(dropDown);
+        Thread.sleep(2000);
+
+        //Get all the options of the dropdown
+        select.getAllSelectedOptions();
+        Thread.sleep(2000);
+
+        //Options size
+        List<WebElement> selectList = select.getOptions();
+
+        System.out.println("selectList.size() = " + selectList.size());
+        Thread.sleep(2000);
+
+        //Print all test
+        List<WebElement> selectAllList = select.getOptions();
+
+        for (WebElement element : selectAllList) {
+
+            System.out.println("element.getText() = " + element.getText());
+        }
+        Thread.sleep(2000);
+
+        //Verify the dropdown has option "Black"
+        List <WebElement> dropDownList = select.getOptions();
+
+        for (WebElement element : dropDownList) {
+
+            if(element.getText().contains("Black")){
+                System.out.println("dropDownList has Black elements");
+            }
+        }
+        Thread.sleep(2000);
+
+        //Print FirstSelectedOptionTest
+        System.out.println("select.getFirstSelectedOption().getText() = "
+                + select.getFirstSelectedOption().getText());
+        Thread.sleep(2000);
+
+        //Select option "Yellow"
+
+        select.selectByVisibleText("Yellow");
+
+    }
 
 }

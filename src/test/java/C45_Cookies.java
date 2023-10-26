5b799eadce6231609ac1e65dfe3eecd4480d0fb5
ZeynepA <zeyneppaytop@gmail.com>
2023-10-26 21:32:57 +0300

    CWJUnit Project 26.10.2023

--- src/test/java/C45_Cookies.java
+++ src/test/java/C45_Cookies.java
@@ -1,13 +1,17 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Cookie;
 import utilities.TestBase;
+
+import java.util.Set;
 
 public class C45_Cookies extends TestBase {
 
     //***Cookie cookie  = driver.manage().cookie-methods
     //Go to URL: https://kitchen.applitools.com/ingredients/cookie
-    //Get Cookie.
-    //Find the total number of cookies.
     //Print all the cookies.
+    // Get Cookie.
+    //Find the total number of cookies.
     //Add Cookie.
     //Edit Cookie.
     //Delete Cookie.
@@ -15,6 +19,61 @@ public class C45_Cookies extends TestBase {
     @Test
     public void test1() {
 
+        driver.get("https://kitchen.applitools.com/ingredients/cookie");
+
+       Set<Cookie> allCookies = driver.manage().getCookies();
+
+        for (Cookie c : allCookies) {
+
+            System.out.println(c);
+        }
+
+        System.out.println("************************");
+        System.out.println(driver.manage().getCookieNamed("protein"));
+
+        Assert.assertEquals("chicken", driver.manage().getCookieNamed("protein").getValue());
+
+        System.out.println("******************************");
+
+        System.out.println(allCookies.size());
+
+        Cookie cookie = new Cookie("kalsiyum","süt");
+        driver.manage().addCookie(cookie);
+
+        allCookies = driver.manage().getCookies();
+
+        for (Cookie c : allCookies) {
+
+            System.out.println(c);
+        }
+
+        System.out.println("************************");
+
+        Cookie cookie2 = new Cookie("vegetable","cucumber");
+        driver.manage().addCookie(cookie2);
+
+        allCookies = driver.manage().getCookies();
+
+        for (Cookie c : allCookies) {
+
+            System.out.println(c);
+        }
+
+        System.out.println("************************");
+
+        driver.manage().deleteCookie(cookie2);
+
+        allCookies = driver.manage().getCookies();
+
+        for (Cookie c : allCookies) {
+
+            System.out.println(c);
+        }
+
+        System.out.println("************************");
+
+        driver.manage().deleteAllCookies();
+        System.out.println("************************");
 
     }
 }

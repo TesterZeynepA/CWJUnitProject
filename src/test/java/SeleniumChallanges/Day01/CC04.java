2db9099deaa4fdf736209cfb558266d879da7fed
ZeynepA <zeyneppaytop@gmail.com>
2023-10-28 17:39:52 +0300

    CWJUnit Project 28.10.2023

--- src/test/java/SeleniumChallanges/Day01/CC04.java
+++ src/test/java/SeleniumChallanges/Day01/CC04.java
@@ -1,6 +1,12 @@
 package SeleniumChallanges.Day01;
 
-public class CC04 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class CC04 extends TestBase {
 
      /*
         go to https://the-internet.herokuapp.com/geolocation
@@ -8,5 +14,18 @@ public class CC04 {
         verify the button location
         */
 
+    @Test
+    public void test(){
+
+        driver.get("https://the-internet.herokuapp.com/geolocation");
+
+        WebElement whereAmIButton= driver.findElement(By.xpath("//button[@onclick='getLocation()']"));
+
+        Assert.assertEquals(whereAmIButton,driver.findElement(By.xpath("//button[@onclick='getLocation()']")));
+
+
+
+
+    }
 
 }

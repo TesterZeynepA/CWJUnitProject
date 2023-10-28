7f59967d2726bf8eafee570e60b5bfda69d9150f
ZeynepA <zeyneppaytop@gmail.com>
2023-10-28 18:22:31 +0300

    CWJUnit Project 28.10.2023

---
+++ src/test/java/Homework/Task28.java
@@ -0,0 +1,43 @@
+package Homework;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class Task28 extends TestBase {
+/*
+    1) Open the browser
+2) Enter the URL “http://practice.automationtesting.in/”
+            3) Click on Shop Menu
+4) Now click on Home menu button
+5) Test whether the Home page has Three Sliders only
+6) The Home page must contains only three sliders
+*/
+
+    @Test
+    public void test(){
+
+        driver.get("http://practice.automationtesting.in/");
+
+        WebElement shopMenu= driver.findElement(By.linkText("Shop"));
+        shopMenu.click();
+
+       driver.switchTo().frame("aswift_7");
+       WebElement reklamClose= driver.findElement(By.cssSelector("#dismiss-button"));
+       reklamClose.click();
+
+       WebElement homeButton= driver.findElement(By.xpath("//*[contains(text(),'Home')]"));
+       homeButton.click();
+
+       List<WebElement> sliders= driver.findElements(By.xpath("//class[contains('slider')]"));
+        System.out.println(sliders.size());
+
+        Assert.assertEquals(sliders.size(),3);
+
+
+    }
+}
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -38,7 +39,11 @@ public abstract class TestBase {
     @Before
     public void setup(){
 
-       driver= new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-popup-blocking");
+        driver = new ChromeDriver(options);
+
+      // driver= new ChromeDriver();
      // driver= new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));

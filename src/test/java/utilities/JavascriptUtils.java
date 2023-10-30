4fd02d0aa465f59f8b37fdd139687430bc66bec7
ZeynepA <zeyneppaytop@gmail.com>
2023-10-30 17:40:07 +0300

    CWJUnit Project 30.10.2023

--- src/test/java/utilities/JavascriptUtils.java
+++ src/test/java/utilities/JavascriptUtils.java
@@ -5,39 +5,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class JavascriptUtils {
-    protected static WebDriver driver;
-
     //This method will takes two parameter: WebElement, and WebDriver
     //When you pass the element, JS will click on that element
-    public static void clickElementByJS(WebElement element) {
+    public static void clickElementByJS(WebDriver driver,WebElement element) {
         JavascriptExecutor jsexecutor = ((JavascriptExecutor) driver);
         jsexecutor.executeScript("arguments[0].click();", element);
     }
 
     //to get the page title with JS
-    public static String getTitleByJS() {
+    public static String getTitleByJS(WebDriver driver) {
         JavascriptExecutor jsexecutor = ((JavascriptExecutor) driver);
         String title = jsexecutor.executeScript("return document.title;").toString();
         return title;
     }
 
     //Scrolling all the way down
-    public static void scrollDownByJS() {
+    public static void scrollDownByJS(WebDriver driver) {
         JavascriptExecutor jsexecutor = ((JavascriptExecutor) driver);
         jsexecutor.executeScript("window.scrollTo(0,document.body.scrollHeight)");
     }
     //    Scroll al the way up of a page
-    public static void scrollAllUpByJS(){
+    public static void scrollAllUpByJS(WebDriver driver){
         JavascriptExecutor js = (JavascriptExecutor)driver;
         js.executeScript("window.scrollTo(0,-document.body.scrollHeight)");
     }
     //Scroll into view with JS. THIS IS VERY USEFULL
     public static void scrollIntoViewJS(WebDriver driver, WebElement element) {
-        JavascriptExecutor jsexecutor = (JavascriptExecutor) JavascriptUtils.driver;
+        JavascriptExecutor jsexecutor = (JavascriptExecutor) driver;
         jsexecutor.executeScript("arguments[0].scrollIntoView();", element);
     }
 
-    public static void changeBackgroundColorByJS(String color, WebElement element) {
+    public static void changeBackgroundColorByJS(WebDriver driver,WebElement element, String color) {
         JavascriptExecutor javascriptExecutor = ((JavascriptExecutor) driver);
         javascriptExecutor.executeScript("arguments[0].style.backgroundColor = '" + color + "'", element);
         try {
@@ -48,15 +46,15 @@ public class JavascriptUtils {
     }
     //Flashing the background color
     // https://www.rapidtables.org/tr/web/color/RGB_Color.html  bu siteden renk ayari yapilablir..Kirmizi- Yesil -Mavi
-    public static void flash(WebElement element) {
+    public static void flash(WebDriver driver, WebElement element) {
         String bgColor = element.getCssValue("backgroundcolor");
         for (int i = 0; i < 5; i++) {
-            changeBackgroundColorByJS("rgb(0,200,0", element);
-            changeBackgroundColorByJS(bgColor, element);
+            changeBackgroundColorByJS(driver,element,"rgb(0,200,0");
+            changeBackgroundColorByJS(driver, element, bgColor);
         }
     }
     //this will generate an alert when needed
-    public static void generateAlert(String message) throws InterruptedException {
+    public static void generateAlert(WebDriver driver,String message) throws InterruptedException {
         JavascriptExecutor javascriptExecutor = ((JavascriptExecutor) driver);
         javascriptExecutor.executeScript("alert('" + message + "')");
         Thread.sleep(3000);
@@ -65,14 +63,14 @@ public class JavascriptUtils {
     /*
      * executes the given JavaScript command on given web element
      */
-    public static void executeJScommand(WebElement element, String command) {
+    public static void executeJScommand(WebDriver driver,WebElement element, String command) {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript(command, element);
     }
     /*
      * executes the given JavaScript command on given web element
      */
-    public static void executeJScommand(String command) {
+    public static void executeJScommand(WebDriver driver,String command) {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript(command);
     }
@@ -81,13 +79,13 @@ public class JavascriptUtils {
     //    Set the value of an input using js executor. Params: WebElement element, String text
 //    This method changes the value attribute of an element.
 //    It changes the input text
-    public static void setValueByJS(WebElement element, String text){
+    public static void setValueByJS(WebDriver driver,WebElement element, String text){
         JavascriptExecutor js = (JavascriptExecutor)driver;
         js.executeScript("arguments[0].setAttribute('value','"+text+"')",element);
     }
 
     //    get the value of an input. param: idOfElement
-    public static void getValueByJS(String idOfElement){
+    public static void getValueByJS(WebDriver driver,String idOfElement){
         JavascriptExecutor js = (JavascriptExecutor)driver;
         String value=js.executeScript("return document.getElementById('"+idOfElement+"').value").toString();
         System.out.println(value);
@@ -99,7 +97,7 @@ public class JavascriptUtils {
 //        I have to do this, cause getText in this case does not return the text in an input
     }
 
-    public static void addBorderWithJS(WebElement element, String borderStyle){
+    public static void addBorderWithJS(WebDriver driver,WebElement element, String borderStyle){
         JavascriptExecutor js = (JavascriptExecutor)driver;
         js.executeScript("arguments[0].style.border='"+borderStyle+"'",element);
     }

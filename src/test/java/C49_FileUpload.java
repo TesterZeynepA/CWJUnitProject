9329f5136ec1b0d07cffb98daf6a0f9819fdbe23
ZeynepA <zeyneppaytop@gmail.com>
2023-10-30 21:16:00 +0300

    CWJUnit Project 30.10.2023

--- src/test/java/C49_FileUpload.java
+++ src/test/java/C49_FileUpload.java
@@ -1,4 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
 public class C49_FileUpload extends TestBase {
@@ -10,7 +13,15 @@ public class C49_FileUpload extends TestBase {
     public void test1() {
         driver.get("https://the-internet.herokuapp.com/upload");
 
+        WebElement dosyaSec= driver.findElement(By.id("file-upload"));
+        //C:\Users\zeyne\IdeaProjects\CWJUnitProject\FileExist
+        String path = System.getProperty("user.dir")+System.getProperty("file.separator")+"FileExist";
+        dosyaSec.sendKeys(path);
 
+        driver.findElement(By.id("file-submit")).click();
 
+        WebElement successMessage= driver.findElement(By.tagName("h3"));
+
+        Assert.assertTrue(successMessage.isDisplayed());
     }
 }

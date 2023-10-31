e4a540ff6b5d01e89e3a1515daf726dc36594b2f
ZeynepA <zeyneppaytop@gmail.com>
2023-10-31 15:24:11 +0300

    CWJUnit Project 31.10.2023

--- src/test/java/Homework/Task29.java
+++ src/test/java/Homework/Task29.java
@@ -1,13 +1,25 @@
 package Homework;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
 
-public class Task29 {
+public class Task29 extends TestBase {
     //Go to URL: https://www.monsterindia.com/seeker/registration
 //Upload file.
 
     @Test
     public void test(){
+
+        driver.get("https://www.monsterindia.com/seeker/registration");
+
+        WebElement fileUpload= driver.findElement(By.xpath("//input[@accept='application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document']"));
+
+        String path = System.getProperty("user.dir")+System.getProperty("file.separator")+"FileExist";
+
+        fileUpload.sendKeys(path);
+
 
 
 
--- src/test/java/Homework/Task30.java
+++ src/test/java/Homework/Task30.java
@@ -1,5 +1,13 @@
 package Homework;
 
+import org.apache.poi.ss.usermodel.*;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Task30 {
 
     //Store the path of the file as string and open the file.
@@ -15,8 +23,39 @@ public class Task30 {
     //Close the file.
     //Close the workbook.
 
+
 //dosya bu clasttan once 3. sutun temiz olmalı.. ilk calıstırıldıgında yazar..
 // 2. kez calıstırılınca uzerine yazar, hata vermez
 
+     @Test
+    public void test() throws IOException {
+         //C:\Users\zeyne\IdeaProjects\CWJUnitProject\src\test\resources\homework-30.xlsx
 
+         String path = System.getProperty("user.dir")+ "\\src\\test\\resources\\homework-30.xlsx";
+
+         FileInputStream fis = new FileInputStream(path);
+
+         Workbook wb = WorkbookFactory.create(fis);
+
+         Sheet sheet = wb.getSheet("Sheet1");
+         Row row = sheet.createRow(0);
+         Cell r1c3= row.createCell(2);
+         r1c3.setCellValue("POPULATION");
+
+         Row row2 = sheet.createRow(1);
+         Cell r2c3= row2.createCell(2);
+         r2c3.setCellValue("data");
+
+         sheet.createRow(2).createCell(2).setCellValue("data");
+
+         sheet.createRow(3).createCell(2).setCellValue("data");
+
+         FileOutputStream fos = new FileOutputStream(path);
+         wb.write(fos);
+
+         wb.close();
+         fis.close();
+         fos.close();
+
+    }
 }

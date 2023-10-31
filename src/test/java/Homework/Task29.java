3dd64cc6b6f9b6c2e744ac397adc390ae6748b74
ZeynepA <zeyneppaytop@gmail.com>
2023-10-31 14:42:56 +0300

    CWJUnit Project 31.10.2023

---
+++ src/test/java/Homework/Task29.java
@@ -0,0 +1,16 @@
+package Homework;
+
+import org.junit.Test;
+
+public class Task29 {
+    //Go to URL: https://www.monsterindia.com/seeker/registration
+//Upload file.
+
+    @Test
+    public void test(){
+
+
+
+
+    }
+}
---
+++ src/test/java/Homework/Task30.java
@@ -0,0 +1,22 @@
+package Homework;
+
+public class Task30 {
+
+    //Store the path of the file as string and open the file.
+    //Open the workbook.
+    //Open the first worksheet.
+    //Go to the first row.
+    //Create a cell on the 3rd column (2nd index) on the first row.
+    //Write "POPULATION" on that cell.
+    //Create a cell on the 2nd row 3rd cell(index2), and write data.
+    //Create a cell on the 3rd row 3rd cell(index2), and write data.
+    //Create a cell on the 4th row 3rd cell(index2), and write data.
+    //Write and save the workbook.
+    //Close the file.
+    //Close the workbook.
+
+//dosya bu clasttan once 3. sutun temiz olmalı.. ilk calıstırıldıgında yazar..
+// 2. kez calıstırılınca uzerine yazar, hata vermez
+
+
+}

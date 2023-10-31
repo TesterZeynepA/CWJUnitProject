5b7cab22afb56168877f115fb2bdbc03b10f276b
ZeynepA <zeyneppaytop@gmail.com>
2023-10-31 22:35:35 +0300

    CWJUnit Project 31.10.2023

--- src/test/java/C55_WebTables.java
+++ src/test/java/C55_WebTables.java
@@ -1,4 +1,30 @@
-public class C55_WebTables {
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C55_WebTables extends TestBase {
+
+//<table>, bir web tablosunu tanımlamak için kullanılan HTML etiketidir.
+//Tablonun başlığını tanımlamak için <th> kullanılırken ,
+// web tablosu için sırasıyla satırları ve sütunları tanımlamak için <tr> ve <td> etiketleri kullanılır.
+//table//tbody//tr  tbody içindeki tüm satırları döndürür.
+//table//tbody//tr[1]  tbody'deki ilk satırı döndürür.
+//table//tbody//tr[1]//td  İlk satırdaki tüm tablo hücrelerini döndürür.
+//table//tbody//tr[1]//td[4]  tbody'deki ilk satırdaki 4. hücreyi döndürür.
+//table//tbody//tr[4]//td[5]  4. satır 5. sütun
+//table//tbody//tr[10]//td[2]  Satır 10 data 2
+//table//tbody//tr//td[5]  5. sütundaki tüm satırları döndürür.
+
+//Go to URL: http://demo.guru99.com/test/web-table-element.php
+//To find third row of table
+//To get 3rd row's 2nd column data
+//Get all the values of a Dynamic Table
+
+    @Test
+    public void test(){
 
 
+
+
+
+    }
 }

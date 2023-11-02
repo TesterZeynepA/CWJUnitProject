4a609a90fc762a3d2303132b9d3792de088f7cfb
ZeynepA <zeyneppaytop@gmail.com>
2023-11-02 14:40:43 +0300

    CWJUnit Project 02.11.2023

--- src/test/java/AutomationExercise/TestCase_4.java
+++ src/test/java/AutomationExercise/TestCase_4.java
@@ -1,6 +1,9 @@
 package AutomationExercise;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
 public class TestCase_4 extends TestBase {
@@ -21,6 +24,38 @@ public class TestCase_4 extends TestBase {
     @Test
     public void test(){
         driver.get("http://automationexercise.com");
+
+        WebElement homePage = driver.findElement(By.xpath("//i[@class='fa fa-home']"));
+
+        Assert.assertTrue(homePage.isDisplayed());
+
+        WebElement loginPage = driver.findElement(By.xpath("//i[@class='fa fa-lock']"));
+        loginPage.click();
+
+        WebElement loginMessage = driver.findElement(By.xpath("//*[contains(text(),'Login to your account')]"));
+
+        Assert.assertTrue(loginMessage.isDisplayed());
+
+        WebElement loginEmail= driver.findElement(By.xpath("//input[@data-qa='login-email']"));
+        loginEmail.sendKeys("azes1@gmail.com");
+
+        WebElement loginPassword= driver.findElement(By.xpath("//input[@data-qa='login-password']"));
+        loginPassword.sendKeys("123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@data-qa='login-button']"));
+        loginButton.click();
+
+        WebElement loginInAsUsername = driver.findElement(By.xpath("//i[@class='fa fa-user']"));
+
+        Assert.assertTrue(loginInAsUsername.isDisplayed());
+
+        WebElement logOut= driver.findElement(By.xpath("//i[@class='fa fa-lock']"));
+        logOut.click();
+
+        WebElement loginPage2 = driver.findElement(By.xpath("//i[@class='fa fa-lock']"));
+
+        Assert.assertTrue(loginPage2.isDisplayed());
+
 
 
     }

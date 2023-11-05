877d4fb4d12808d912ab26958313ca094b671434
ZeynepA <zeyneppaytop@gmail.com>
2023-11-06 00:38:42 +0300

    CWJUnit Project 06.11.2023

--- src/test/java/AutomationExercise/TestCase_8.java
+++ src/test/java/AutomationExercise/TestCase_8.java
@@ -1,5 +1,6 @@
 package AutomationExercise;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -18,7 +19,8 @@ public class TestCase_8 extends TestBase {
       6. The products list is visible
       7. Click on 'View Product' of first product
       8. User is landed to product detail page
-      9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
+      9. Verify that detail detail is visible: product name, category, price,
+       availability, condition, brand
      */
 
     @Test
@@ -33,7 +35,34 @@ public class TestCase_8 extends TestBase {
         WebElement products= driver.findElement(By.xpath("//i[@class='material-icons card_travel']"));
         products.click();
 
+        driver.switchTo().frame("aswift_6");
+
+        driver.switchTo().frame("ad_iframe");
+
+        WebElement reklam= driver.findElement(By.id("dismiss-button"));
+
+        reklam.click();
+
         Assert.assertTrue(driver.findElement(By.xpath("//h2[@class='title text-center']")).getText().contains("ALL PRODUCTS"));
+
+        Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='nav nav-pills nav-stacked']")).isDisplayed());
+
+        driver.findElement(By.xpath("//a[@href='/product_details/1']")).click();
+
+        WebElement productName= driver.findElement(By.xpath("//h2[contains(text(),'Blue Top')]"));
+        WebElement category= driver.findElement(By.xpath("//p[contains(text(),'Category: Women > Tops')]"));
+        WebElement price = driver.findElement(By.xpath("//span[contains(text(),'Rs. 500')]"));
+        WebElement availability= driver.findElement(By.xpath("//b[contains(text(),'Availability:')]"));
+        WebElement condition = driver.findElement(By.xpath("//b[contains(text(),'Condition:')]"));
+        WebElement brand = driver.findElement(By.xpath("//b[contains(text(),'Brand:')]"));
+
+        Assert.assertTrue(productName.isDisplayed());
+        Assert.assertTrue(category.isDisplayed());
+        Assert.assertTrue(price.isDisplayed());
+        Assert.assertTrue(availability.isDisplayed());
+        Assert.assertTrue(condition.isDisplayed());
+        Assert.assertTrue(brand.isDisplayed());
+
 
     }
 }
